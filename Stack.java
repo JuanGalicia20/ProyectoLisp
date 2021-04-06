445dcba1f131104613c1d8d7f0c6391387260743
Juan Andrés Galicia <69065800+JuanGalicia20@users.noreply.github.com>
2021-04-05 22:47:10 -0600

    Javadoc

--- Stack.java
+++ Stack.java
@@ -1,12 +1,14 @@
 import java.util.ArrayList;
 import java.util.Vector;
-
-
 /**
-*
-* @author Juan Andres Galicia Reyes 20298 UVG 2021
-* @version 06/02/21
-*/
+ * @author Juan Andres Galicia 20298
+ * @author Elisa Samayoa 20710
+ * @author Jonathan Espinoza 20022
+ * 
+ * @version 5-4-2021 
+ * 
+ *  Implementacion de la interfaz stack
+ */
 public class Stack<E>
 implements IStack<E>
 {
@@ -18,6 +20,10 @@ implements IStack<E>
 		data = new Vector<E>(10,2);
 	}
 
+	
+	/** 
+	 * @param item
+	 */
 	public void push(E item)
 	// post: the value is added to the stack
 	//          will be popped next if no intervening push
@@ -25,6 +31,10 @@ implements IStack<E>
 		data.add(item);
 	}
 
+	
+	/** 
+	 * @return E
+	 */
 	public E pop()
 	// pre: stack is not empty
 	// post: most recently pushed item is removed and returned
@@ -32,6 +42,10 @@ implements IStack<E>
 		return data.remove(size()-1);
 	}
 
+	
+	/** 
+	 * @return E
+	 */
 	public E peek()
 	// pre: stack is not empty
 	// post: top value (next to be popped) is returned
@@ -39,12 +53,20 @@ implements IStack<E>
 		return data.get(size() - 1);
 	}
 	
+	
+	/** 
+	 * @return int
+	 */
 	public int size()
 	// post: returns the number of elements in the stack
 	{
 		return data.size();
 	}
  
+	
+	/** 
+	 * @return boolean
+	 */
 	public boolean empty()
 	// post: returns true if and only if the stack is empty
 	{
