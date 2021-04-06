c1558859dd5fa6573b30450ab227e8976d548121
Juan Andrés Galicia <69065800+JuanGalicia20@users.noreply.github.com>
2021-04-05 22:48:02 -0600

    Javadoc

--- verificar.java
+++ verificar.java
@@ -1,22 +1,32 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.List;
 import java.util.ArrayList;
-//import com.sun.org.apache.xalan.internal.xsltc.compiler.Pattern;
-
-
-
-public class verificar {
+import java.util.LinkedHashMap;
+/**
+ * @author Juan Andres Galicia 20298
+ * @author Elisa Samayoa 20710
+ * @author Jonathan Espinoza 20022
+ * 
+ * @version 5-4-2021 
+ * 
+ * 
+ */
+public class Verificar {
 
     private String[] codigo2;
     private String codigo;
-    public verificar(String codigo)
+    public Verificar(String codigo)
     {
         this.codigo = codigo;
         codigo2 = codigo.replace("("," ( ").replace(")"," ) ").split(" ");
     }
-    
 
+    public Verificar()
+    {
+
+    }
+
+    /** 
+     * @return boolean
+     */
     //Verifica que sea par el numero de parentesis
     public boolean verif1()
     {
@@ -59,117 +69,56 @@ public class verificar {
         return pila.empty();
     }
 
-        
 
-    public void verifOperacion()
+
+    
+    /** 
+     * @param codigo
+     * @return boolean
+     */
+    public boolean VerifList(ArrayList<ArrayList<String>> codigo)
     {
-        char c1 = codigo.charAt(1);
-        char op1 = '+';     
-        char op2 = '-';  
-        char op3 = '/';  
-        char op4 = '*';  
-        if(c1 == op1 || c1 == op2 || c1 == op3 || c1 == op4)
-        {
-            System.out.println("OPERACION VALIDA");
-            verifFuncion();
+        boolean hasWord = false;
+        for(int i = 0; i < codigo.size(); i++) {
+            for(int j =0; j<codigo.get(i).size();j++)
+            {
+                String currentWord = codigo.get(i).get(j);
+                if(currentWord.equals("first") || currentWord.equals("second")
+                || currentWord.equals("third") || currentWord.equals("fourth") || currentWord.equals("fifth")
+                || currentWord.equals("sixth") || currentWord.equals("seventh")|| currentWord.equals("eighth")
+                || currentWord.equals("ninth") || currentWord.equals("tenth") || currentWord.equals("nth"))
+                {
+                    hasWord=true;
+                }
+            }
         }
-        else
-        {
-            System.out.println("OPERACION MALA");
-            verifFuncion();
-        }
-    }
-
-    public String verifFuncion(){
-        String[] letras = codigo.split("");
-        String ver = (letras[1] + letras[2] + letras[3] + letras [4] + letras[5]);
-        if(ver.equals("DEFUN") || ver.equals("defun")){
-            System.out.println("Declaracion de funcion valida");
-            return codigo;
-        }
-        else{
-            System.out.println("Funcion no valida");
-            return codigo;
-        }
-        
-    }
-
-    public String verifQuote(){
-        String[] letras = codigo.split("");
-        String ver = (letras[0] + letras[1] + letras[2] + letras[3] + letras [4] + letras[5]); //verificación por si el usuario lo ingresa con paréntesis
-        if(ver.equals("(QUOTE") || ver.equals("(quote")){ 
-            System.out.println("Declaracion de quote valida");
-            return codigo;
-        }
-        else if(letras[1].equals("('")){
-            System.out.println("Declaracion de quote valida");
-            return codigo;
-        }
-
-        String ver2 = (letras[0] + letras[1] + letras[2] + letras[3] + letras [4] ); //verficiación si no se escribe con paréntesis
-           
-        else if(ver.equals("QUOTE") || ver.equals("quote")){
-            System.out.println("Declaracion de quote valida");
-            return codigo;
-        }
-        else if(letras[0].equals("'")){
-            System.out.println("Declaracion de quote valida");
-            return codigo;
-
-        else{
-            System.out.println("quote no valida");
-            return codigo;
-        }
-        
-    }
-
-    public void verifList (){
-        String[] letras = codigo.split("");
-        String ver = (letras[0] + letras[1] + letras[2] + letras[3] + letras [4] ); //verificación por si el usuario lo ingresa con paréntesis
-        int size= letras.length;
-        if(ver.equalsIgnoreCase("(list")){ 
-            System.out.println("Declaracion de list valida");
-            return codigo;           
-        }
-        else{
-            System.out.println("list no valida");
-            return codigo;
+        return hasWord;
     }
 
     
-    public String verifSetQ(){
-        String[] letras = codigo.split("");
-        String ver = (letras[1] + letras[2] + letras[3] + letras [4]);
-        if(ver.equals("SETQ") || ver.equals("SETQ")){
-            System.out.println("Declaracion de SETQ valida");
-            return codigo;
-        }
-        else{
-            System.out.println("SETQ no valida");
-            return codigo;
-        }
-        
-    }
-
-    public String verfAtom(){
-        String[] letras = codigo.split("");
-        String ver = (letras[1] + letras[2] + letras[3] + letras [4]);
-        if(ver.equals("ATOM") || ver.equals("atom")){
-            System.out.println("Declaracion de ATOM valida");
-            return codigo;
-
-
-            for(int i = 0; i < codigo.length(); i++){
-                codigo[i] + codigo{i+1}.....
+    /** 
+     * @param codigo
+     * @param LinkedHashMap<String
+     * @param variables
+     * @return boolean
+     */
+    public boolean VerifVariable(ArrayList<ArrayList<String>> codigo, LinkedHashMap<String, ArrayList<String>> variables)
+    {
+        for(String element : variables.keySet())
+        {
+            for(int i = 0; i < codigo.size(); i++) {
+                for(int j =0; j<codigo.get(i).size();j++)
+                {
+                    if(element.equals(codigo.get(i).get(j)))
+                    {
+                        return true;
+                    }
+                }
             }
         }
-        else{
-            System.out.println("ATOM no valida");
-            return codigo;
-        }
-        
-    }
 
+        return false;
+    }
 
 }
 
